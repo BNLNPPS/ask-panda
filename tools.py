6b0400badd60ec228d2368a0e8ec5e4fe28995f5
PalNilsson <Paul.Nilsson@cern.ch>
2025-07-11 10:23:21 +0200

    Corrected logging file handler name

--- tools.py
+++ tools.py
@@ -33,7 +33,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(levelname)s: %(message)s',
     handlers=[
-        logging.FileHandler("error_analyzer.log"),
+        logging.FileHandler("tools.log"),
         logging.StreamHandler()
     ]
 )
